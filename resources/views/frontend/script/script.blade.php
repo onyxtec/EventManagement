7674c092996f106c3b77d2c5b33ea760f2963390
faa-onyxtec <faa@onyxtec>
2022-06-02 15:49:51 +0500

    mobile view responsive

--- resources/views/frontend/script/script.blade.php
+++ resources/views/frontend/script/script.blade.php
@@ -1,14 +1,12 @@
 <script src={{asset('/frontend/vendor/aos/aos.js')}}></script>
 <script src={{asset('/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}></script>
-<script src={{asset('/frontend/vendor/glightbox/js/glightbox.min.js')}}></script>
 <script src={{asset('/frontend/vendor/isotope-layout/isotope.pkgd.min.js')}}></script>
-<script src={{asset('/frontend/vendor/swiper/swiper-bundle.min.js')}}></script>
-<script src={{asset('/frontend/vendor/php-email-form/validate.js')}}></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- Template Main JS File -->
 <script src={{asset('/frontend/js/main.js')}}></script>
+<script src={{asset('/frontend/js/script.js')}}></script>
 
 </body>
 
